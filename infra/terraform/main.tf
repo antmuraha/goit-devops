83eee244a166b5a8732addf9374653e69ebd2d69
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2026-06-10 03:03:37 +0300

    fix: update S3 bucket name and availability zones in main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,7 +1,13 @@
+# Terraform versions
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+
 # Connect S3 and DynamoDB modules
 module "s3_backend" {
   source      = "./modules/s3-backend"
-  bucket_name = "l5-bucket"
+  bucket_name = "goit-l5-bucket"
   table_name  = "terraform-locks"
 }
 
@@ -11,13 +17,13 @@ module "vpc" {
   vpc_cidr_block     = "10.0.0.0/16"
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  availability_zones = ["us-east-1"]
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
   vpc_name           = "lesson-5-vpc"
 }
 
 # Connect ECR module
 module "ecr" {
-  source      = "./modules/ecr"
+  source = "./modules/ecr"
   # ecr_name    = "lesson-5-ecr"
 
   repository_name      = "lesson-5-ecr"
