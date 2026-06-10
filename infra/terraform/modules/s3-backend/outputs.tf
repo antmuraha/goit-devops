f19c44aa99ea673f0498c51bc4d3d715fa033804
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2026-06-10 03:02:30 +0300

    feat: add outputs for S3 bucket and DynamoDB table in outputs.tf

---
+++ infra/terraform/modules/s3-backend/outputs.tf
@@ -0,0 +1,17 @@
+output "bucket_name" {
+  value       = aws_s3_bucket.terraform_state.bucket
+  description = "Name of the S3 bucket storing Terraform state"
+}
+
+output "bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "ARN of the Terraform state S3 bucket"
+}
+
+output "bucket_url" {
+  value = "https://${aws_s3_bucket.terraform_state.bucket}.s3.amazonaws.com"
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.name
+}
