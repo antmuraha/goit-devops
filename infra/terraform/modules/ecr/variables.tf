f54316fd39389b1e87e324ffa2b0b3e7e1d8723c
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2026-06-10 03:03:10 +0300

    feat: add ECR repository configuration and outputs in Terraform

--- infra/terraform/modules/ecr/variables.tf
+++ infra/terraform/modules/ecr/variables.tf
@@ -1,7 +1,10 @@
 variable "repository_name" {
-  type = string
+  type        = string
+  description = "Name of the ECR repository"
 }
 
 variable "image_tag_mutability" {
-  type = string
+  type        = string
+  default     = "MUTABLE"
+  description = "Tag mutability setting for the ECR repository (MUTABLE or IMMUTABLE)"
 }
