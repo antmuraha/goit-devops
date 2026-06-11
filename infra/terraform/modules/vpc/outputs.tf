73dc454345ed3f8c6b48395b987ee0464a13655a
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2026-06-11 03:31:20 +0300

    feat: add NAT Gateway and private route table configuration for VPC

--- infra/terraform/modules/vpc/outputs.tf
+++ infra/terraform/modules/vpc/outputs.tf
@@ -27,3 +27,13 @@ output "public_route_table_id" {
   value       = aws_route_table.public.id
   description = "ID of the public route table"
 }
+
+# NAT Gateway
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat.id
+}
+
+# Private route table
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
