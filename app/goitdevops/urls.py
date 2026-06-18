7edf3bb2641c0e231ee84347c08951e6aaa5c623
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2026-06-18 22:24:49 +0300

    debug: update debug version to 1.0.3 in urls.py

--- app/goitdevops/urls.py
+++ app/goitdevops/urls.py
@@ -22,7 +22,7 @@ def health(request):
     return JsonResponse({"status": "ok"})
 
 def debug(request):
-    return JsonResponse({"version": "1.0.2", "status": "ok"})
+    return JsonResponse({"version": "1.0.3", "status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
